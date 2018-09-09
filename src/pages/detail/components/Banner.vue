4a3f815acbe4bc7c2b29dd07a0c24213d27de7a7
radar112 <radarwolf112@163.com>
2018-09-09 11:14:53 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+      class="banner-img"
+      src="http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_140x140_1c863e5c.jpg"
+      alt="">
+      <div class="banner-info">
+        <div class="banner-tittle">北京市朝阳区欢乐谷</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe678;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClick"
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary.vue'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg', 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClick () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      display flex
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-tittle
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .32rem
+        line-height .32rem
+        margin-top .14rem
+        padding 0 .4rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
